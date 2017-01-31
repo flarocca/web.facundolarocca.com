b61a0b62126b5d2175011aab4589311ddc5c9d60
Facundo La Rocca <frocca@web.web.com>
2017-01-31 17:31:21 -0300

    Responsive + Firefox + IE changes

--- src/components/App.js
+++ src/components/App.js
@@ -50,22 +50,31 @@ export default class App extends Component {
     return (
       <div>
         <MediaQuery query='(min-device-width: 1224px)'>
-          <div id="app" className="App column" style={{ color: "dimgray", backgroundImage: `url(${BackgroundImage})`, backgroundRepeat: "repeat" }}>
-            <Element name="TOP" />
-            <div id="main" className="Container column co-medium" style={{ alignSelf: "center", backgroundColor: "transparent" }}>
-              <div className="Container row jc-right" style={{ paddingRight: "1%" }}>
-                <a target="_blank" className="Container column jc-center" href={ImageSources.STACKOVERFLOW_PROFILE}><StackOverflow className="icon-med" innerColor="dimgray" outerColor="transparent" /></a>
-                <a target="_blank" className="Container column jc-center" href={ImageSources.LINKEDIN_PROFILE}><LinkedIn className="icon-large" innerColor="dimgray" outerColor="transparent" /></a>
-                <a target="_blank" className="Container column jc-center" href={ImageSources.GITHUB_PROFILE}><GitHub className="icon" innerColor="dimgray" outerColor="transparent" /></a>
-              </div>
-              <div style={{ backgroundColor: this.state.theme.BACKGROUND_COLOR }}>
-                <AppHeader languageSet={this.state.languageSet} theme={this.state.theme} />
-                <AppBody languageSet={this.state.languageSet} theme={this.state.theme} />
-                <AppFooter languageSet={this.state.languageSet} theme={this.state.theme} />
-                <UpButton theme={this.state.theme} />
+          <MediaQuery query='(max-width: 480px)'>
+            <div id="app-mobile" className="App-mobile column" style={{ backgroundColor: this.state.theme.BACKGROUND_COLOR }}>
+              <AppHeaderMobile languageSet={this.state.languageSet} theme={this.state.theme} />
+              <AppBodyMobile languageSet={this.state.languageSet} theme={this.state.theme} />
+              <AppFooterMobile languageSet={this.state.languageSet} theme={this.state.theme} />
+            </div>
+          </MediaQuery>
+          <MediaQuery query='(min-width: 480px)'>
+            <div id="app" className="App column" style={{ color: "dimgray", backgroundImage: `url(${BackgroundImage})`, backgroundRepeat: "repeat" }}>
+              <Element name="TOP" />
+              <div id="main" className="Container column co-medium" style={{ alignSelf: "center", backgroundColor: "transparent" }}>
+                <div className="Container row jc-right" style={{ paddingRight: "1%" }}>
+                  <a target="_blank" className="Container column jc-center" href={ImageSources.STACKOVERFLOW_PROFILE}><StackOverflow className="icon-med" innerColor="dimgray" outerColor="transparent" /></a>
+                  <a target="_blank" className="Container column jc-center" href={ImageSources.LINKEDIN_PROFILE}><LinkedIn className="icon-large" innerColor="dimgray" outerColor="transparent" /></a>
+                  <a target="_blank" className="Container column jc-center" href={ImageSources.GITHUB_PROFILE}><GitHub className="icon" innerColor="dimgray" outerColor="transparent" /></a>
+                </div>
+                <div style={{ backgroundColor: this.state.theme.BACKGROUND_COLOR }}>
+                  <AppHeader languageSet={this.state.languageSet} theme={this.state.theme} />
+                  <AppBody languageSet={this.state.languageSet} theme={this.state.theme} />
+                  <AppFooter languageSet={this.state.languageSet} theme={this.state.theme} />
+                  <UpButton theme={this.state.theme} />
+                </div>
               </div>
             </div>
-          </div>
+          </MediaQuery>
         </MediaQuery>
         <MediaQuery query='(max-device-width: 1224px)'>
           <div id="app-mobile" className="App-mobile column" style={{ backgroundColor: this.state.theme.BACKGROUND_COLOR }}>
