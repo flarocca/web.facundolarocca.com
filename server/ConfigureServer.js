49fd67acd4446b0e51d66229e21dfee71b35c777
Facundo La Rocca <frocca@web.web.com>
2017-02-24 17:07:22 -0300

    redux-calculator site added

--- server/ConfigureServer.js
+++ server/ConfigureServer.js
@@ -7,8 +7,7 @@ var ConfigureServer = (server, express, PORT) => {
     server.use(bodyParser.urlencoded({ extended: true }));
     server.use(bodyParser.json());
     server.use((req, res, next) => {
-        //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + PORT);
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + PORT);
         res.setHeader('Access-Control-Allow-Methods', 'POST');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
         next();
---
+++ server/routes/ExampleRoutes.js
@@ -0,0 +1,13 @@
+
+let Routes = require('./Routes');
+
+class ExampleRoutes extends Routes {
+    _addAllRoutes(server) {
+        server.get('/examples/redux-calculator', (req, res) => {
+            res.sendFile(__dirname + '/redux-calculator/index.html')
+        });
+    }
+
+}
+
+module.exports = ExampleRoutes;
