c32200f32853b7492f6077718e1ee8cfb4a60681
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2017-04-15 19:05:00 -0300

    LinkedIn url changed

--- src/constants/ImageSources.js
+++ src/constants/ImageSources.js
@@ -1,5 +1,5 @@
 var ImageSources = {
-  LINKEDIN_PROFILE: 'https://www.linkedin.com/in/facundo-la-rocca-5803673b?authType=NAME_SEARCH&authToken=fc8W&locale=es_ES&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A141225072%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1482027341667%2Ctas%3Afacundo%20la%20rocca',
+  LINKEDIN_PROFILE: 'https://www.linkedin.com/in/facundo-la-rocca-5803673b',
   GITHUB_PROFILE: 'https://github.com/flarocca',
   STACKOVERFLOW_PROFILE: 'http://stackoverflow.com/users/4585153/facundo-la-rocca?tab=profile',
   REACT: 'https://facebook.github.io/react/',
